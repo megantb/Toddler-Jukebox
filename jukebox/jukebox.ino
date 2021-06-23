458099be73c20e3791d3494ead9353e3cf3eb296
megantb <megantbutler@gmail.com>
2021-06-22 21:00:46 -0500

    Update jukebox.ino

--- jukebox/jukebox.ino
+++ jukebox/jukebox.ino
@@ -6,9 +6,16 @@
 #include <SPI.h>
 
 TMRpcm tmrpcm;
+const int buttonPin = 2;
+
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
 
 void setup()
 {
+pinMode(buttonPin, INPUT);
+
 tmrpcm.speakerPin=9;
 Serial.begin(9600);
 if(!SD.begin(SD_ChipSelectPin))
@@ -16,14 +23,21 @@ if(!SD.begin(SD_ChipSelectPin))
   Serial.println("SD fail");
   return;
 }
+tmrpcm.quality(1);
 tmrpcm.setVolume(6);
-tmrpcm.play((char*)"Daniel.wav");
+
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  play();
+
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    play();
+  }
 
 }
 
